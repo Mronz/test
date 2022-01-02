17bdc2d2d695f0105f5fed5bb0ba2f479f02f03f
Mronz <kamil.2003.kmz@gmail.com>
2022-01-03 00:32:38 +0100

    kolejna zmiana

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express()
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 app.get("/", function (req, res) {
-    res.send("<h1>My app on heroku! - zmiana w pliku</h1>")
+
+    form = `<form action="/" method="GET">
+            <input type="submit" value="przejdź do /data">
+            </form>`
+    res.send("<h1>My app on heroku! - zmiana w pliku</h1>" + form)
 })
 
 app.get("/data", function (req, res) {
